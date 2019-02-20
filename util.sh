ac0c527500586d4c9d3f165005818bedacf4dfd6
anlongfei <1056432250@qq.com>
2019-02-20 17:33:47 +0800

    hello world

---
+++ util.sh
@@ -0,0 +1,43 @@
+work_dir=/Users/alf/git-file/openresty/work
+bin_vec=(``)
+bin_vec[0]="stop"
+stop() {
+    echo "stop"
+    openresty -p ${work_dir} -s stop
+}
+bin_vec[1]="start"
+start() {
+    echo "start"
+    openresty -p ${work_dir} -c ${work_dir}/conf/nginx.conf
+}
+bin_vec[2]="info"
+info() {
+    echo "info"
+    netstat -nlt | grep 8080
+    lsof -i tcp:8080
+}
+bin_vec[3]="restart"
+restart() {
+    echo "restart"
+    openresty -p ${work_dir} -s stop
+    openresty -p ${work_dir} -c ${work_dir}/conf/nginx.conf
+
+}
+bin_name=$1
+help=1
+for((i=0;i<${#bin_vec[*]};i++))
+do
+    if [ "${bin_vec[$i]}" = "$bin_name" ];then
+        shift 1
+        help=0
+        ${bin_vec[$i]}
+    fi
+done
+
+if [ $help -eq 1 ];then
+    echo "opt is :"
+    for((i=0;i<${#bin_vec[*]};i++))
+    do
+        echo "[${bin_vec[$i]}]"
+    done
+fi
